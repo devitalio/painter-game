d7ad492378e6b912a370a5439f58158521ffd02b
devitalio <devitalio@users.noreply.github.com>
2017-12-06 12:41:05 +0200

    init the balls
    Change-Id: Ie2fd4e8d39733b337b3c2dd4a8d9377869ebd447

--- game/ball.js
+++ game/ball.js
@@ -3,14 +3,26 @@
 var ball = {};
 
 ball.initialize = function () {
-   
+    ball.position = { x: 0, y: 0 };
+    ball.velocity = { x: 0, y: 0 };
+    ball.origin = { x: 0, y: 0 };
+    ball.currentColor = sprites.ball_red;
+    ball.isShooting = false;
 };
 
 ball.update = function (delta) { };
-ball.reset = function () { };
+
+ball.reset = function() {
+    ball.position = { x: 0, y: 0 };
+    ball.isShooting = false;
+};
 
 ball.draw = function () {
-    
+    if (!ball.isShooting) {
+        return;
+    }
+
+    Canvas2D.drawImage(ball.currentColor, ball.position, ball.rotation, ball.origin);
 }
 
 ball.handleInput = function (delta) {
