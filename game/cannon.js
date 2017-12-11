d7393b9812c463f61dc433d5203df01b9702d28e
devitalio <devitalio@users.noreply.github.com>
2017-12-11 17:07:20 +0200

    colors
    Change-Id: I1b7758a30f4a4d852e2b78dc8f9534776bb37c57

--- game/cannon.js
+++ game/cannon.js
@@ -5,6 +5,7 @@ function Cannon () {
     this.colorPosition = new Vector2(55, 388);
     this.origin =  new Vector2(34, 34);
     this.currentColor = sprites.cannon_red;
+    this.color = Color.red;
     this.rotation = 0;
 };
 
@@ -35,4 +36,32 @@ Cannon.prototype.ballPosition = function () {
     var opposite = Math.sin(this.rotation) * sprites.cannon_barrel.width * 0.8;
     var adjacent = Math.cos(this.rotation) * sprites.cannon_barrel.width * 0.8;
     return new Vector2( this.position.x + adjacent, this.position.y + opposite);
+};
+
+Cannon.prototype.getColor = function () {
+    if (this.currentColor === sprites.cannon_red) {
+        return Color.red;
+    }
+
+    if (this.currentColor === srites.cannon_blue) {
+        return Color.blue;
+    }
+
+    if (this.currentColor === srites.cannon_green) {
+        return Color.green;
+    }
+};
+
+Cannon.prototype.setColor = function (value) {
+    if (value === Color.red) {
+        this.currentColor = sprites.cannon_red
+    }
+
+    if (value === Color.blue) {
+        this.currentColor = sprites.cannon_blue;
+    }
+
+    if (value === Color.green) {
+        this.currentColor = sprites.cannon_green;
+    }
 };
