16e8bc49d814b2e6af1d069a21a19272a986bde2
devitalio <devitalio@users.noreply.github.com>
2017-12-12 15:01:41 +0200

    some properties on cannon
    Change-Id: I0cfaf9ac4b070c12b2105fe789b446d04586d512

--- game/cannon.js
+++ game/cannon.js
@@ -2,21 +2,21 @@
 
 function Cannon () {
     this.position = new Vector2(72, 405);
-    this.colorPosition = new Vector2(55, 388);
     this.origin =  new Vector2(34, 34);
     this.currentColor = sprites.cannon_red;
-    this.color = Color.red;
     this.rotation = 0;
 };
 
 Cannon.prototype.update = function (delta) { };
-Cannon.prototype.reset = function() {
+
+Cannon.prototype.reset = function () {
     this.position = new Vector2(72, 405);
 };
 
 Cannon.prototype.draw = function () {
+    var colorPosition = this.position.subtract(this.size.divideBy(2));
     Canvas2D.drawImage(sprites.cannon_barrel, this.position, this.rotation, this.origin);
-    Canvas2D.drawImage(this.currentColor, this.colorPosition, 0, new Vector2());
+    Canvas2D.drawImage(this.currentColor, colorPosition, 0, new Vector2());
 }
 
 Cannon.prototype.handleInput = function (delta) {
@@ -25,43 +25,76 @@ Cannon.prototype.handleInput = function (delta) {
     this.rotation = Math.atan2(opposite, adjacent);
 
     if (Keyboard.key === "g")
-        this.currentColor = sprites.cannon_green;
+        this.color = Color.green;
     if (Keyboard.key === "r")
-        this.currentColor = sprites.cannon_red;
+        this.color = Color.red;
     if (Keyboard.key === "b")
-        this.currentColor = sprites.cannon_blue;
+        this.color = Color.blue;
 };
 
-Cannon.prototype.ballPosition = function () {
-    var opposite = Math.sin(this.rotation) * sprites.cannon_barrel.width * 0.8;
-    var adjacent = Math.cos(this.rotation) * sprites.cannon_barrel.width * 0.8;
-    return new Vector2( this.position.x + adjacent, this.position.y + opposite);
-};
+Object.defineProperty(Cannon.prototype, "center",
+    {
+        get: function() {
+            return new Vector2(this.width / 2, this.height / 2);
+        }
+    });
 
-Cannon.prototype.getColor = function () {
-    if (this.currentColor === sprites.cannon_red) {
-        return Color.red;
+Object.defineProperty(Cannon.prototype, "size",
+{
+    get: function() {
+        return new Vector2(this.currentColor.width, this.currentColor.height);
     }
+});
 
-    if (this.currentColor === srites.cannon_blue) {
-        return Color.blue;
+Object.defineProperty(Cannon.prototype, "height",
+{
+    get: function() {
+        return this.currentColor.height;
     }
+});
 
-    if (this.currentColor === srites.cannon_green) {
-        return Color.green;
+Object.defineProperty(Cannon.prototype, "width",
+{
+    get: function () {
+        return this.currentColor.width;
     }
-};
+});
 
-Cannon.prototype.setColor = function (value) {
-    if (value === Color.red) {
-        this.currentColor = sprites.cannon_red
+Object.defineProperty(Cannon.prototype, "ballPosition",
+{
+    get: function() {
+        var opposite = Math.sin(this.rotation) * sprites.cannon_barrel.width * 0.8;
+        var adjacent = Math.cos(this.rotation) * sprites.cannon_barrel.width * 0.8;
+        return new Vector2(this.position.x + adjacent, this.position.y + opposite);
     }
+});
 
-    if (value === Color.blue) {
-        this.currentColor = sprites.cannon_blue;
-    }
+Object.defineProperty(Cannon.prototype,"color",
+    {
+        get: function() {
+            if (this.currentColor === sprites.cannon_red) {
+                return Color.red;
+            }
 
-    if (value === Color.green) {
-        this.currentColor = sprites.cannon_green;
-    }
-};
+            if (this.currentColor === srites.cannon_blue) {
+                return Color.blue;
+            }
+
+            if (this.currentColor === srites.cannon_green) {
+                return Color.green;
+            }
+        },
+        set: function() {
+            if (value === Color.red) {
+                this.currentColor = sprites.cannon_red
+            }
+
+            if (value === Color.blue) {
+                this.currentColor = sprites.cannon_blue;
+            }
+
+            if (value === Color.green) {
+                this.currentColor = sprites.cannon_green;
+            }
+        }
+});
