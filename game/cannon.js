d48f55699b11bd02445617639d7cb2552aae7d13
devitalio <devitalio@users.noreply.github.com>
2017-12-14 17:20:09 +0200

    base class and cannon refactor
    Change-Id: If8340b0d4c736f82f6e7f74fcfefe7423d86149b

--- game/cannon.js
+++ game/cannon.js
@@ -1,11 +1,12 @@
 "use strict";
 
-function Cannon () {
+function Cannon() {
+    ThreeColorGameObject.call(this, sprites.cannon_red, sprites.cannon_green, sprites.cannon_blue); //superclass constructor call
     this.position = new Vector2(72, 405);
     this.origin =  new Vector2(34, 34);
-    this.currentColor = sprites.cannon_red;
-    this.rotation = 0;
 };
+
+Cannon.prototype = Object.create(ThreeColorGameObject.prototype);
 
 Cannon.prototype.update = function (delta) { };
 
@@ -14,9 +15,12 @@ Cannon.prototype.reset = function () {
 };
 
 Cannon.prototype.draw = function () {
+    if (!this.visible)
+        return;
+
     var colorPosition = this.position.substract(this.size.divide(2));
     Canvas2D.drawImage(sprites.cannon_barrel, this.position, this.rotation, this.origin);
-    Canvas2D.drawImage(this.currentColor, colorPosition, 0, new Vector2());
+    Canvas2D.drawImage(this.currentColor, colorPosition);
 }
 
 Cannon.prototype.handleInput = function (delta) {
@@ -32,33 +36,6 @@ Cannon.prototype.handleInput = function (delta) {
         this.color = Color.blue;
 };
 
-Object.defineProperty(Cannon.prototype, "center",
-    {
-        get: function() {
-            return new Vector2(this.width / 2, this.height / 2);
-        }
-    });
-
-Object.defineProperty(Cannon.prototype, "size",
-{
-    get: function() {
-        return new Vector2(this.currentColor.width, this.currentColor.height);
-    }
-});
-
-Object.defineProperty(Cannon.prototype, "height",
-{
-    get: function() {
-        return this.currentColor.height;
-    }
-});
-
-Object.defineProperty(Cannon.prototype, "width",
-{
-    get: function () {
-        return this.currentColor.width;
-    }
-});
 
 Object.defineProperty(Cannon.prototype, "ballPosition",
 {
@@ -67,34 +44,4 @@ Object.defineProperty(Cannon.prototype, "ballPosition",
         var adjacent = Math.cos(this.rotation) * sprites.cannon_barrel.width * 0.8;
         return new Vector2(this.position.x + adjacent, this.position.y + opposite);
     }
-});
-
-Object.defineProperty(Cannon.prototype,"color",
-    {
-        get: function() {
-            if (this.currentColor === sprites.cannon_red) {
-                return Color.red;
-            }
-
-            if (this.currentColor === sprites.cannon_blue) {
-                return Color.blue;
-            }
-
-            if (this.currentColor === sprites.cannon_green) {
-                return Color.green;
-            }
-        },
-        set: function(value) {
-            if (value === Color.red) {
-                this.currentColor = sprites.cannon_red
-            }
-
-            if (value === Color.blue) {
-                this.currentColor = sprites.cannon_blue;
-            }
-
-            if (value === Color.green) {
-                this.currentColor = sprites.cannon_green;
-            }
-        }
 });
