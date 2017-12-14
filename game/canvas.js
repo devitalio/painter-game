8056f2d0b15f0a294145c73a0865909887a385a8
devitalio <devitalio@users.noreply.github.com>
2017-12-14 16:37:10 +0200

    canvas refactor
    Change-Id: I7c5a93ab56d707cc22995034550670047910e8c9

--- game/canvas.js
+++ game/canvas.js
@@ -1,29 +1,39 @@
 "use strict";
 
-var Canvas2D = {
-    canvas: undefined,
-    canvasContext: undefined
+function Canvas2D_Singleton() {
+    console.log("Creating Canvas2D object");
+    this.canvas = null;
+    this.canvasContext = null;
 }
 
-Canvas2D.clear = function () {
+Canvas2D_Singleton.prototype.initialize = function (canvasName) {
+    this.canvas = document.getElementById(canvasName);
+
+    if (this.canvas.getContext)
+        this.canvasContext = this.canvas.getContext('2d');
+    else {
+        alert('Your browser is not HTML5 compatible.!');
+    }
+};
+
+Canvas2D_Singleton.prototype.clear = function () {
     this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
-}
+};
 
-Canvas2D.drawImage = function (sprite, position, rotation, origin) {
-
+Canvas2D_Singleton.prototype.drawImage = function (sprite, position, rotation, origin, scale) {
     position = typeof position !== 'undefined' ? position : Vector2.zero;
     rotation = typeof rotation !== 'undefined' ? rotation : 0;
-    origin = typeof origin !== 'undefined' ? origin : Vector2.zero
+    scale = typeof scale !== 'undefined' ? scale : 1;
+    origin = typeof origin !== 'undefined' ? origin : Vector2.zero;
 
     this.canvasContext.save();
     this.canvasContext.translate(position.x, position.y);
     this.canvasContext.rotate(rotation);
-    this.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height,
-        -origin.x, -origin.y, sprite.width, sprite.height);
+    this.canvasContext.drawImage(sprite, 0, 0,
+        sprite.width, sprite.height,
+        -origin.x * scale, -origin.y * scale,
+        sprite.width, sprite.height * scale);
     this.canvasContext.restore();
-}
+};
 
-Canvas2D.initialize = function(canvas) {
-    Canvas2D.canvas = document.getElementById(canvas);
-    Canvas2D.canvasContext = Canvas2D.canvas.getContext('2d');
-}
+var Canvas2D = new Canvas2D_Singleton();
